614572c81a54ead44d445b363cf24c43062ed9bd
azraa4 <93424530+azraa4@users.noreply.github.com>
2022-01-12 12:50:23 +0300

    Add files via upload

---
+++ step.ino
@@ -0,0 +1,22 @@
+
+// defines pins numbers
+const int stepPin = 3; 
+const int dirPin = 2; 
+
+void setup() {
+  // Sets the two pins as Outputs
+  pinMode(stepPin,OUTPUT); 
+  pinMode(dirPin,OUTPUT);
+}
+void loop() {
+  digitalWrite(dirPin,HIGH); // Enables the motor to move in a particular direction
+  // Makes 200 pulses for making one full cycle rotation
+  for(int x = 0; x < 1600; x++) {
+    digitalWrite(stepPin,HIGH); 
+    delayMicroseconds(500); 
+    digitalWrite(stepPin,LOW); 
+    delayMicroseconds(500); 
+  }
+  delay(5000); // One second delay
+  
+}
